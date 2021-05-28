ef0be38e0b47953cac3ef3c6ddfddc66ee458342
swanious <ohsuwan1@gmail.com>
2021-05-28 13:40:03 +0900

    remove key

--- config/config/settings.py
+++ config/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm1e#ql)o-um@5)aucll9v(#z0@wn&3fc&6y3x4$f4*%p*s+oe_'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
